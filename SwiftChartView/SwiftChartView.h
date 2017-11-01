f9ce61e4fb9c820e5ec0616de0a604d626fa768a
zhoudengfeng <zhoudengfeng8@gmail.com>
2017-11-01 10:37:38 +0800

    First commit

---
+++ SwiftChartView/SwiftChartView.h
@@ -0,0 +1,19 @@
+//
+//  SwiftChartView.h
+//  SwiftChartView
+//
+//  Created by ZHOU DENGFENG on 1/11/17.
+//  Copyright © 2017 ZHOU DENGFENG DEREK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftChartView.
+FOUNDATION_EXPORT double SwiftChartViewVersionNumber;
+
+//! Project version string for SwiftChartView.
+FOUNDATION_EXPORT const unsigned char SwiftChartViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftChartView/PublicHeader.h>
+
+
---
+++ SwiftChartViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftChartViewDemo
+//
+//  Created by ZHOU DENGFENG on 1/11/17.
+//  Copyright © 2017 ZHOU DENGFENG DEREK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
